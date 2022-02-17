c69b6a74fe22edc3669f00f6b0c3ae423284e105
Rubén Zubicoa <ruben.zubicoa@empro-ms.com>
2022-02-17 17:00:45 +0100

    feat: add publish func

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+    "time"
 )
 
 var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
@@ -23,8 +24,18 @@ func sub(client mqtt.Client) {
     fmt.Printf("Subscribed to topic %s", topic)
 }
 
+func publish(client mqtt.Client) {
+    num := 10
+    for i := 0; i < num; i++ {
+        text := fmt.Sprintf("Message %d", i)
+        token := client.Publish("topic/test", 0, false, text)
+        token.Wait()
+        time.Sleep(time.Second)
+    }
+}
+
 func main(){
-	var broker = "broker.emqx.io"
+	var broker = "127.0.0.1"
 	var port = 1883
 	opts := mqtt.NewClientOptions()
     opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
@@ -39,4 +50,6 @@ func main(){
         panic(token.Error())
   }
   sub(client)
+  publish(client)
+  client.Disconnect(250)
 }
